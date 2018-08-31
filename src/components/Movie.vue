15a7a73de7b45543a9a1e9ebb398dcb2b4f06725
Hippolyte-Fayol Toulassi <Hippolyte Toulassi>
2018-08-31 21:43:35 +0200

    movie display completed

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,11 +1,4 @@
 <template>
-    <!--<div class="media">
-            <div class="columns is-multiline">
-                <figure v-for="(facet, index) in movie.actor_facets" :key="index">
-                    <img v-bind:src="facet.split('|')[0]">
-                </figure>
-            </div>
-        </div> -->
     <div class="column is-one-third">
         <div class="card large round">
             <div class="card-image">
@@ -15,24 +8,27 @@
             </div>
             <div class="card-content">
                 <div class="media">
-                    <!-- <div class="media-left">
-                            <figure class="image is-96x96">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg" alt="Image">
-                            </figure>
-                        </div> -->
                     <div class="media-content">
                         <p class="title is-4 no-padding">{{movie.title}}</p>
-                        <p><span v-for="(genre, index) in movie.genre" :key="index" class="title is-6"> {{genre}} | </span></p>
-                        <p class="subtitle is-6">{{movie.rating}} | {{movie.score}}</p>
+                        <div class="field is-grouped">
+                            <div class="tags">
+                                <span class="tag is-primary" v-for="(genre, index) in movie.genre" :key="index" v-if="index<movie.genre.length -1">{{genre}}</span>
+                                <span class="tag is-primary">{{movie.genre[movie.genre.length-1]}}</span>
+                            </div>
+                        </div>
+                        <div class="tags has-addons level-item">
+                            <span class="tag is-rounded is-warning">{{movie.rating}}</span>
+                            <span class="tag is-rounded">{{movie.year}}</span>
+                        </div>
                     </div>
                     <div class="media-right">
-                        <a class="button delete is-large is-warning" v-on:click="handleDelete(movie.objectID)"></a>
+                        <a class="button delete is-large is-dark" v-on:click="handleDelete(movie.objectID)"></a>
                     </div>
                 </div>
-                <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam
-                    tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis
-                    totam! Labore reprehenderit beatae magnam animi!
+                <div class="media level-item has-text-centered">
+                    <figure v-for="(facet, index) in movie.actor_facets" :key="index" class="tooltip" v-bind:data-tooltip="facet.split('|')[1]" v-if="index<8">
+                        <img class="author-image" v-bind:src="facet.split('|')[0]">
+                    </figure>
                 </div>
             </div>
         </div>
@@ -67,7 +63,13 @@
 </script>
 
 <style scoped>
-    .image {
-        height: 50%;
+    .author-image {
+        top: -30px;
+        left: 50%;
+        width: 60px;
+        height: 50px;
+        margin-left: -15px;
+        border: 3px solid #ccc;
+        border-radius: 50%;
     }
 </style>
